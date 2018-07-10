e3b727fd7ccbc5dd476ed990c2ce9f9a5d3a3bf4
August Duet <august.duet@revature.com>
2018-07-10 00:31:51 -0400

    add some important output

--- src/main/java/com/ex/Main.java
+++ src/main/java/com/ex/Main.java
@@ -3,5 +3,6 @@ package com.ex;
 public class Main {
     public static void main(String[] args) {
         System.out.println("hello world!");
+        System.out.println("Some important output");
     }
 }
